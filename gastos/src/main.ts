a30e9a6d0f05baba505075485905e9fe8b94a909
camilo <ecgarciap@slabcode.com>
2024-09-16 14:17:34 -0500

    estructura

---
+++ gastos/src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/es";
+import "@quasar/extras/material-icons/material-icons.css";
+import router from "./router";
+
+const myApp = createApp(App);
+const pinia = createPinia();
+
+myApp.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+});
+myApp.use(router);
+myApp.use(pinia);
+myApp.mount("#app");
